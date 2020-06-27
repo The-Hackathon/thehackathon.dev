07ef666952d6d5667514f431b23d386dbdec7376
Gabriel Poça <gabrielpoca@gmail.com>
2020-06-27 10:02:02 +0100

    Live logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -11,54 +11,102 @@ const range = (start, end, increment = 1) => {
   return items
 }
 
+const diffDates = (date1, date2) => {
+  return date2.getTime() - date1.getTime()
+}
+
 const LogoSVG = styled.svg`
   margin-bottom: 2rem;
 `
 
-export const Logo = () => {
-  if (typeof window !== "object") return null
+export class Logo extends React.Component {
+  constructor() {
+    super()
+    this.els = {}
+  }
 
-  return (
-    <LogoSVG width="160" height="160" viewBox="-180 -180 340 340">
-      {range(-10, 9).map(j =>
-        range(-10, 9).map(i =>
-          Math.max(Math.abs(i), Math.abs(j)) / 9 - Math.random() * 0.7 < 0.4 ? (
+  componentDidMount() {
+    this.time = new Date()
+    this.update()
+  }
+
+  shouldComponentUpdate() {
+    return false
+  }
+
+  update = () => {
+    const newTime = new Date()
+
+    if (diffDates(this.time, newTime) > 150) {
+      range(-10, 9).map(j =>
+        range(-10, 9).map(
+          i =>
+            (this.els[i][j].style.fill =
+              Math.max(Math.abs(i), Math.abs(j)) / 9 - Math.random() * 0.7 < 0.4
+                ? "#fe8019"
+                : "transparent")
+        )
+      )
+
+      this.time = newTime
+    }
+
+    requestAnimationFrame(this.update)
+  }
+
+  saveRef = (el, i, j) => {
+    this.els[i] = this.els[i] || {}
+    this.els[i][j] = el
+  }
+
+  render() {
+    return (
+      <LogoSVG width="160" height="160" viewBox="-180 -180 340 340">
+        {range(-10, 9).map(j =>
+          range(-10, 9).map(i => (
             <rect
+              ref={el => this.saveRef(el, i, j)}
               key={`${i}${j}`}
               width="20"
               height="20"
               x={i * 20}
               y={j * 20}
-              style={{ fill: "#fe8019" }}
+              style={{
+                fill:
+                  Math.max(Math.abs(i), Math.abs(j)) / 9 - Math.random() * 0.7 <
+                  0.4
+                    ? "#fe8019"
+                    : "transparent",
+              }}
               shapeRendering="crispEdges"
             />
-          ) : null
-        )
-      )}
-      <rect
-        width="40"
-        height="160"
-        x="-80"
-        y="-80"
-        style={{ fill: "#1d2021" }}
-        shapeRendering="crispEdges"
-      />
-      <rect
-        width="80"
-        height="40"
-        x="-40"
-        y="-20"
-        style={{ fill: "#1d2021" }}
-        shapeRendering="crispEdges"
-      />
-      <rect
-        width="40"
-        height="160"
-        x="20"
-        y="-80"
-        style={{ fill: "#1d2021" }}
-        shapeRendering="crispEdges"
-      />
-    </LogoSVG>
-  )
+          ))
+        )}
+        <rect
+          width="40"
+          height="160"
+          x="-80"
+          y="-80"
+          style={{ fill: "#1d2021" }}
+          shapeRendering="crispEdges"
+        />
+        <rect
+          width="80"
+          height="40"
+          x="-40"
+          y="-20"
+          style={{ fill: "#1d2021" }}
+          shapeRendering="crispEdges"
+        />
+        <rect
+          width="40"
+          height="160"
+          x="20"
+          y="-80"
+          style={{ fill: "#1d2021" }}
+          shapeRendering="crispEdges"
+        />
+      </LogoSVG>
+    )
+  }
 }
